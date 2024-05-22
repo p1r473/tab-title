6a8f4d8ce13176af6bd79104b02560f77ba93e2d
p1r473 <subwayjared@gmail.com>
2024-05-22 12:40:16 -0400

    Update tab-title.plugin.zsh

--- tab-title.plugin.zsh
+++ tab-title.plugin.zsh
@@ -1,19 +1,26 @@
 update_window_title() {
-    local title_content="$1"
-    if [[ -n "$2" ]]; then
-        title_content="$title_content $2"  # Append command if provided
-    fi
+	username=$(whoami)
+	hostname=$(hostname)
+	local dir="$1"
+	local cmd="$2"
 
-    if [ -n "$STY" ] || [ -n "$SCREEN" ]; then
-        # Screen
-        print -Pn "\ek$title_content\e\\"
-    elif [ -n "$TMUX" ]; then
-        # Tmux 
-	print -Pn "\033]2;$title_content\033\\"    
-        print -Pn "\033k$title_content\033\\"  # Set tmux window name
-        print -Pn "\033]0;$title_content\033\\" # Set terminal title (xterm compatible)
-        tmux rename-window "$title_content"
-    fi
+	# Truncate directory to 20 characters
+	local truncated_dir=$(print -P "%20<..<${dir}")
+	local truncated_title="$truncated_dir"
+
+	if [ -n "$cmd" ]; then
+	    truncated_title="$truncated_dir - $cmd"
+	    pane_content="$username@$hostname - $dir - $cmd"
+	else
+	    pane_content="$username@$hostname - $dir"
+	fi
+
+	print -Pn -- "\033k$truncated_title\033\\"
+	print -Pn -- "\033]2;$pane_content\033\\"
+	print -Pn -- "\033]0;$pane_content\033\\"
+	if [ -n "$TMUX" ]; then
+	    tmux rename-window "$truncated_title"
+	fi
 }
 
 # Set up hooks for changing directory and executing commands
@@ -21,20 +28,20 @@ autoload -Uz add-zsh-hook
 
 # Function to update window title when changing directories
 chpwd_title_update() {
-    local current_dir=$(print -P "%15<..<%~")  # Truncated current directory
+    local current_dir=$(print -P "%~")
     update_window_title "$current_dir"
 }
 
 # Function to capture the command and update title before execution
 preexec_title_update() {
-    local cmd_first_part=${1[(w)1]}  # Store the first word of the command
-    local current_dir=$(print -P "%15<..<%~")  # Truncated current directory
+    local cmd_first_part=${1[(w)1]}
+    local current_dir=$(print -P "%~")
     update_window_title "$current_dir" "$cmd_first_part"
 }
 
 # Function to reset the window title after command finishes
 precmd_title_update() {
-    local current_dir=$(print -P "%15<..<%~")  # Truncated current directory
+    local current_dir=$(print -P "%~")
     update_window_title "$current_dir"
 }
 
